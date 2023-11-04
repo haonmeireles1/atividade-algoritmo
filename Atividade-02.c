38e92623c8dc95d63754fc7a4e2594b710d8e193
Noah Meireles <30630938+haonmeireles1@users.noreply.github.com>
2023-11-04 19:13:45 -0300

    Create Atividade-02.c

---
+++ Atividade-02.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+
+typedef struct {
+    char sexo;
+    float salario;
+} Assalariado;
+
+char validaSexo() {
+    char sexo;
+    do {
+        printf("Informe o sexo (M para masculino, F para feminino): ");
+        scanf(" %c", &sexo);
+    } while(sexo != 'M' && sexo != 'F');
+    return sexo;
+}
+
+float validaSalario() {
+    float salario;
+    do {
+        printf("Informe o salário: ");
+        scanf("%f", &salario);
+    } while(salario <= 1);
+    return salario;
+}
+
+char* classificaSalario(float salario) {
+    if (salario > 1400) {
+        return "Acima";
+    } else if (salario == 1400) {
+        return "Igual";
+    } else {
+        return "Abaixo";
+    }
+}
+
+void mostraClassifica(Assalariado assalariado) {
+    printf("Sexo: %c\n", assalariado.sexo);
+    printf("Salário: R$%.2f\n", assalariado.salario);
+    printf("Classificação em relação ao salário mínimo: %s\n\n", classificaSalario(assalariado.salario));
+}
+
+int main() {
+    int totalAbaixoSalarioMinimo = 0;
+    int totalAcimaSalarioMinimo = 0;
+    Assalariado assalariado;
+
+    while(1) {
+        assalariado.sexo = validaSexo();
+        assalariado.salario = validaSalario();
+        mostraClassifica(assalariado);
+
+        if (assalariado.salario < 1400) {
+            totalAbaixoSalarioMinimo++;
+        } else if (assalariado.salario > 1400) {
+            totalAcimaSalarioMinimo++;
+        }
+    }
+
+    printf("Total de assalariados abaixo do salário mínimo: %d\n", totalAbaixoSalarioMinimo);
+    printf("Total de assalariados acima do salário mínimo: %d\n", totalAcimaSalarioMinimo);
+
+    return 0;
+}
